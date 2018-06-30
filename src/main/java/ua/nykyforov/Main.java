8674e08d23f8e5fffadc6601555dc85c05f7f868
Serhii Nykyforov <04mailbox@gmail.com>
2018-06-30 11:38:26 +0300

    Add slf4j dependency

--- src/main/java/ua/nykyforov/Main.java
+++ src/main/java/ua/nykyforov/Main.java
@@ -1,16 +1,19 @@
 package ua.nykyforov;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.nykyforov.service.QuestionService;
 import ua.nykyforov.service.SimpleQuestionService;
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-context.xml");
         QuestionService questionService = ctx.getBean(SimpleQuestionService.class);
-        System.out.println(questionService.getAllQuestions());
+        logger.info("{}", questionService.getAllQuestions());
     }
 
 }
