fe3359d90a02e1e6a47b70b457b858ef9bd08ee7
Serhii Nykyforov <04mailbox@gmail.com>
2018-07-09 20:01:03 +0300

    Put Main into `service.quiz` package

--- src/main/java/ua/nykyforov/service/quiz/Main.java
+++ src/main/java/ua/nykyforov/service/quiz/Main.java
@@ -1,4 +1,4 @@
-package ua.nykyforov;
+package ua.nykyforov.service.quiz;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
