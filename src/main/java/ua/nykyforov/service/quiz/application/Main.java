dcccdee3cf7a2535ef4d07fa1082cb2688062b3e
Serhii Nykyforov <04mailbox@gmail.com>
2018-07-14 16:13:49 +0300

    Migrate project to spring boot

--- src/main/java/ua/nykyforov/service/quiz/application/Main.java
+++ src/main/java/ua/nykyforov/service/quiz/application/Main.java
@@ -1,13 +1,15 @@
 package ua.nykyforov.service.quiz.application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ua.nykyforov.service.quiz.application.controller.QuizController;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext ctx = SpringApplication.run(Main.class, args);
         QuizController quizController = ctx.getBean(QuizController.class);
         quizController.passTest();
     }
