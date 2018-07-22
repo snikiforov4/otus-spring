e3c6f26aefeb2b156345e6b96ce46587124be9c0
Serhii Nykyforov <04mailbox@gmail.com>
2018-07-22 13:44:03 +0300

    Add command for pass quiz

--- quiz/src/main/java/ua/nykyforov/service/quiz/application/shell/QuizCommands.java
+++ quiz/src/main/java/ua/nykyforov/service/quiz/application/shell/QuizCommands.java
@@ -5,39 +5,38 @@ import org.springframework.context.MessageSource;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import ua.nykyforov.service.quiz.application.config.QuizConfig;
+import ua.nykyforov.service.quiz.core.application.QuizController;
 import ua.nykyforov.service.quiz.core.application.UserService;
 import ua.nykyforov.service.quiz.core.model.User;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 
 @ShellComponent
-public class UserCommands {
+public class QuizCommands {
 
     private final UserService userService;
+    private final QuizController quizController;
     private final MessageSource appMessageSource;
     private final QuizConfig quizConfig;
 
     @Autowired
-    public UserCommands(UserService userService, MessageSource appMessageSource, QuizConfig quizConfig) {
+    public QuizCommands(UserService userService, QuizController quizController,
+                        MessageSource appMessageSource, QuizConfig quizConfig) {
         this.userService = userService;
+        this.quizController = quizController;
         this.appMessageSource = appMessageSource;
         this.quizConfig = quizConfig;
     }
 
-    @ShellMethod("Add new user.")
-    public String addUser(@NotEmpty String firstName, @NotEmpty String lastName) {
-        User user = userService.save(new User(firstName, lastName));
-        return appMessageSource.getMessage("cmd.add.user.result",
-                new Object[]{user.getId()}, quizConfig.getSettings().getLocale());
+    @ShellMethod("Pass quiz with specified user.")
+    public void passQuiz(@Positive long userId) {
+        User user = getUser(userId);
+        quizController.passTest(user);
     }
 
-    @ShellMethod("Find user by id.")
-    public String getUser(@Positive long id) {
+    private User getUser(@Positive long id) {
         return userService.getById(id)
-                .map(User::toString)
-                .orElseGet(() -> appMessageSource.getMessage("user.not-found",
-                        new Object[]{id}, quizConfig.getSettings().getLocale()));
+                .orElseThrow(() -> new IllegalArgumentException(appMessageSource.getMessage("user.not-found",
+                        new Object[]{id}, quizConfig.getSettings().getLocale())));
     }
-
 }
