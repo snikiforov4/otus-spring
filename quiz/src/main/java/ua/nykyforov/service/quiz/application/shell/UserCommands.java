a2aadd8e2f8b0638d48b8da0ef4a895ce77db836
Serhii Nykyforov <04mailbox@gmail.com>
2018-07-22 13:21:04 +0300

    Add l10n to user commands

--- quiz/src/main/java/ua/nykyforov/service/quiz/application/shell/UserCommands.java
+++ quiz/src/main/java/ua/nykyforov/service/quiz/application/shell/UserCommands.java
@@ -1,8 +1,10 @@
 package ua.nykyforov.service.quiz.application.shell;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import ua.nykyforov.service.quiz.application.config.QuizConfig;
 import ua.nykyforov.service.quiz.core.application.UserService;
 import ua.nykyforov.service.quiz.core.model.User;
 
@@ -13,23 +15,29 @@ import javax.validation.constraints.Positive;
 public class UserCommands {
 
     private final UserService userService;
+    private final MessageSource appMessageSource;
+    private final QuizConfig quizConfig;
 
     @Autowired
-    public UserCommands(UserService userService) {
+    public UserCommands(UserService userService, MessageSource appMessageSource, QuizConfig quizConfig) {
         this.userService = userService;
+        this.appMessageSource = appMessageSource;
+        this.quizConfig = quizConfig;
     }
 
     @ShellMethod("Add new user.")
     public String addUser(@NotEmpty String firstName, @NotEmpty String lastName) {
         User user = userService.save(new User(firstName, lastName));
-        return "User was added with ID=" + user.getId();
+        return appMessageSource.getMessage("cmd.add.user.result",
+                new Object[]{user.getId()}, quizConfig.getSettings().getLocale());
     }
 
     @ShellMethod("Find user by id.")
     public String getUser(@Positive long id) {
         return userService.getById(id)
                 .map(User::toString)
-                .orElse(String.format("User with id=%s not found", id));
+                .orElseGet(() -> appMessageSource.getMessage("cmd.get.user.not-found",
+                        new Object[]{id}, quizConfig.getSettings().getLocale()));
     }
 
 }
