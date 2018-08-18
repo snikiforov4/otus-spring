031848576ffec0fc9bb3fd773affb34e4eb4844f
Serhii Nykyforov <04mailbox@gmail.com>
2018-08-18 23:33:13 +0300

    Add work with Genre entity

---
+++ library-mongo/src/main/java/ua/nykyforov/service/library/application/shell/GenreCommands.java
@@ -0,0 +1,28 @@
+package ua.nykyforov.service.library.application.shell;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import ua.nykyforov.service.library.application.domain.Genre;
+import ua.nykyforov.service.library.application.repository.GenreRepository;
+
+import javax.validation.constraints.NotBlank;
+
+@ShellComponent
+@SuppressWarnings("UnusedReturnValue")
+public class GenreCommands {
+
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreCommands(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    @ShellMethod("Add new genre.")
+    Genre addGenre(@NotBlank String name) {
+        Genre genre = new Genre(name);
+        return genreRepository.save(genre);
+    }
+
+}
---
+++ library-mongo/src/test/java/ua/nykyforov/service/library/application/shell/GenreCommandsTest.java
@@ -0,0 +1,45 @@
+package ua.nykyforov.service.library.application.shell;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ua.nykyforov.service.library.application.repository.GenreRepository;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class GenreCommandsTest {
+
+    @Mock
+    private GenreRepository genreRepository;
+
+    private GenreCommands sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new GenreCommands(genreRepository);
+    }
+
+    @Nested
+    @DisplayName("addGenre")
+    class AddGenre {
+
+        @Test
+        void shouldCallRepositoryMethodWithCorrectParams() {
+            String genreName = "Horror";
+
+            sut.addGenre(genreName);
+
+            verify(genreRepository, times(1))
+                    .save(argThat(argument -> genreName.equals(argument.getName())));
+        }
+
+    }
+
+}
