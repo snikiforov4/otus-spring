e8afd9135feb69a29a619b27d0a87872ffe06f91
Serhii Nykyforov <04mailbox@gmail.com>
2018-08-18 23:33:13 +0300

    Add work with Genre entity

--- library-mongo/src/main/java/ua/nykyforov/service/library/application/Main.java
+++ library-mongo/src/main/java/ua/nykyforov/service/library/application/Main.java
@@ -2,8 +2,11 @@ package ua.nykyforov.service.library.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import ua.nykyforov.service.library.application.repository.BookRepository;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = BookRepository.class)
 public class Main {
 
     public static void main(String[] args) {
