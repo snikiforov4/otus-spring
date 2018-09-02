9306fd2c9edf628857ecf67b99ff89f43406814b
Serhii Nykyforov <04mailbox@gmail.com>
2018-09-03 00:04:17 +0300

    Add dialog for adding new tweet

--- twitter-client/src/app/app.component.ts
+++ twitter-client/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {MatDialog} from "@angular/material";
+import {TweetDialogComponent} from "./tweet-dialog/tweet-dialog.component";
+import {Tweet} from "./tweet";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  openComposeNewTweetDialog(): void {
+    const dialogRef = this.dialog.open(TweetDialogComponent, {
+      width: '350px',
+      data: {title: 'Compose new Tweet', actionButtonText: 'Tweet', tweet: new Tweet()}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('New Tweet was successfully added');
+      console.log(result);
+    });
+  }
+
 }
---
+++ twitter-client/src/app/tweet-dialog-data.ts
@@ -0,0 +1,7 @@
+import {Tweet} from "./tweet";
+
+export class TweetDialogData {
+  tweet: Tweet;
+  actionButtonText: string;
+  title: string;
+}
