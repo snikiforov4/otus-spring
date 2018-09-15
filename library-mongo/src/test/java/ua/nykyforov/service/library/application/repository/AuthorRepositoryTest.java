3133bed8fc4fdf66bc8f088f7175de26ccfae408
Serhii Nykyforov <04mailbox@gmail.com>
2018-09-15 14:50:05 +0300

    Use @DataMongoTest for integration tests

--- library-mongo/src/test/java/ua/nykyforov/service/library/application/repository/AuthorRepositoryTest.java
+++ library-mongo/src/test/java/ua/nykyforov/service/library/application/repository/AuthorRepositoryTest.java
@@ -3,7 +3,7 @@ package ua.nykyforov.service.library.application.repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import ua.nykyforov.service.library.application.domain.Author;
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootApplication
+@DataMongoTest
 @SpringJUnitConfig(classes = {MongoConfig.class})
 class AuthorRepositoryTest {
 
@@ -31,17 +31,20 @@ class AuthorRepositoryTest {
     void shouldSaveEntity() {
         String firstName = "Stephen";
         String lastName = "King";
-        Author savedAuthor = sut.save(new Author(firstName, lastName));
-        assertThat(savedAuthor).isNotNull();
-        final String id = savedAuthor.getId();
-        assertThat(id).isNotNull();
 
-        Optional<Author> author = sut.findById(id);
+        Author savedAuthor = sut.save(new Author(firstName, lastName));
+
+        assertThat(savedAuthor).satisfies(a -> {
+            assertThat(a).withFailMessage("saved author").isNotNull();
+            assertThat(a.getId()).withFailMessage("author id").isNotNull();
+        });
+
+        Optional<Author> author = sut.findById(savedAuthor.getId());
 
         assertThat(author)
                 .isPresent()
                 .hasValueSatisfying(g -> {
-                    assertThat(g.getId()).isEqualTo(id);
+                    assertThat(g.getId()).isEqualTo(savedAuthor.getId());
                     assertThat(g.getFirstName()).isEqualTo(firstName);
                     assertThat(g.getLastName()).isEqualTo(lastName);
                 });
--- library-mongo/src/test/java/ua/nykyforov/service/library/application/repository/MongoConfig.java
+++ library-mongo/src/test/java/ua/nykyforov/service/library/application/repository/MongoConfig.java
@@ -2,10 +2,8 @@ package ua.nykyforov.service.library.application.repository;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.test.context.TestPropertySource;
 
 @Configuration
 @EnableMongoRepositories(basePackageClasses = BookRepository.class)
-@TestPropertySource("classpath:application-test.yml")
 public class MongoConfig {
 }
