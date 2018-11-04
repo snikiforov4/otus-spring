2e8c58b7bd017ad33b1e9b0aa33c53ba6df63b54
Serhii Nykyforov <04mailbox@gmail.com>
2018-11-04 19:07:26 +0200

    Add shell commands stub class

---
+++ migrate-tool/src/main/java/ua/nykyforov/migratetool/shell/MigrateCommands.java
@@ -0,0 +1,13 @@
+package ua.nykyforov.migratetool.shell;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class MigrateCommands {
+
+    @ShellMethod("Start migration.")
+    public void start() {
+    }
+
+}
